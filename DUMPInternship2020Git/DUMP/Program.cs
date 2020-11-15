a236d60ca24ecc4b509ad46802e69cb564e6447b
kbtomic <ktomic01@fesb.hr>
2020-11-15 19:52:43 +0100

    Added changes from branch

--- DUMPInternship2020Git/DUMP/Program.cs
+++ DUMPInternship2020Git/DUMP/Program.cs
@@ -7,7 +7,7 @@ namespace DUMP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello from second commit!");
+            Console.WriteLine("Conflict from branch!");
             Console.WriteLine("Hello from third commit!");
         }
     }
